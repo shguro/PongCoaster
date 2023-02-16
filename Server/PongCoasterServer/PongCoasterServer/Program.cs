343d7515204fb104c3eadd83fee596c9d78e2964
Tobias Becker <shguro@users.noreply.github.com>
2023-02-16 19:58:47 +0100

    Simple server and client working.

--- Server/PongCoasterServer/PongCoasterServer/Program.cs
+++ Server/PongCoasterServer/PongCoasterServer/Program.cs
@@ -1,3 +1,22 @@
-﻿using static PongCoasterServer.MqttSimpleServer;
+﻿using System.Text;
+using PongCoasterMqtt;
 
-var runServerWithLogging = Run_Server_With_Logging();
+
+var server = new MqttSimpleServer();
+await server.StartAsync();
+
+var client = new MqttSimpleClient();
+await client.ConnectAsync("localhost", 1883);
+await client.SubscribeAsync("weight/#");
+client.MessageReceived += (sender) =>
+{
+    Console.WriteLine($"Received message on topic {sender.ApplicationMessage.Topic}: {Encoding.UTF8.GetString(sender.ApplicationMessage.Payload)}");
+    return Task.CompletedTask;
+};
+await client.PublishAsync("example/topic", "Hello, world!");
+
+
+Console.WriteLine("Press Enter to exit.");
+Console.ReadLine();
+await client.DisconnectAsync();
+await server.StopAsync();
